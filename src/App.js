b2882cd579a4d11e0113910cafdc38b1167563a1
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-03-27 11:56:08 -0300

    added Home screen in App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
-import SplashScreen from './screens/SplashScreen';
+import { HomeScreen } from './screens';
 import { theme } from './styles/theme';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <SplashScreen />
+      <HomeScreen />
     </ThemeProvider>
   );
 };
