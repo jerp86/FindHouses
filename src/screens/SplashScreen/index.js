44198db88629e94ab1173435a35f93cb4ddb8993
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-03-30 20:18:43 -0300

    added navigation from Splash to Home screen

--- src/screens/SplashScreen/index.js
+++ src/screens/SplashScreen/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { SectionView, SectionImage } from './styles';
 
 import logo from '../../assets/img/findHousesLogo.png';
 
 export const SplashScreen = () => {
+  const navigator = useNavigation();
+  const goToHomeScreen = (time = 3000) => {
+    setTimeout(() => {
+      navigator.navigate('Home');
+    }, time);
+  };
+
+  useEffect(() => goToHomeScreen());
+
   return (
     <SectionView>
       <StatusBar translucent backgroundColor="transparent" />
