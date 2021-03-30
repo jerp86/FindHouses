4a08ed2cd3d270bf1920e7a475acc55b0c88d700
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-03-30 07:01:58 -0300

    added initial configuration of react navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
+import { NavigationContainer } from '@react-navigation/native';
 
 import { HomeScreen } from './screens';
 import { theme } from './styles/theme';
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar translucent backgroundColor={theme.colors.backgroundDark} />
-      <HomeScreen />
-    </ThemeProvider>
+    <NavigationContainer>
+      <ThemeProvider theme={theme}>
+        <StatusBar translucent backgroundColor={theme.colors.backgroundDark} />
+        <HomeScreen />
+      </ThemeProvider>
+    </NavigationContainer>
   );
 };
 
