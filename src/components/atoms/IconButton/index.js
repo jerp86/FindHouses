047b0295cf5f9cfee552d67c82ad6a267cebc345
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-04-05 14:39:19 -0300

    added function in IconButton

--- src/components/atoms/IconButton/index.js
+++ src/components/atoms/IconButton/index.js
@@ -3,9 +3,9 @@ import Icon from 'react-native-vector-icons/Ionicons';
 
 import { IconButtonContainer } from './styles';
 
-export const IconButton = ({ iconName, transparent }) => {
+export const IconButton = ({ iconName, transparent, onPress }) => {
   return (
-    <IconButtonContainer transparent={transparent}>
+    <IconButtonContainer transparent={transparent} onPress={onPress}>
       <Icon name={iconName} color="white" size={20} />
     </IconButtonContainer>
   );
