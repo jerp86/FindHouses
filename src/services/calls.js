a1fc93617cec62c4ad428ca1fdbcc39b16c72869
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-04-10 16:09:38 -0300

    updated calls: added params

--- src/services/calls.js
+++ src/services/calls.js
@@ -1,14 +1,30 @@
 import api from './api';
 
-export const getHousesCall = async () => {
+export const getHousesCall = async query => {
+  const {
+    sizeMin,
+    sizeMax,
+    priceMin,
+    priceMax,
+    bedsMin,
+    bathsMin,
+    offset,
+  } = query;
+
   try {
     const result = await api.get('/properties/v2/list-for-rent', {
       params: {
         city: 'Miami',
         state_code: 'FL',
-        limit: 5,
-        offset: 0,
+        limit: 15,
+        offset,
         sort: 'relevance',
+        sqft_min: sizeMin,
+        sqft_max: sizeMax,
+        price_min: priceMin,
+        price_max: priceMax,
+        beds_min: bedsMin,
+        baths_min: bathsMin,
       },
     });
 
