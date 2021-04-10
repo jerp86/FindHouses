09fd5c7c0777c63483fd4d22db2f2f878118d1b3
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-04-10 16:08:19 -0300

    updated HousesList organism

--- src/components/organisms/HousesList/index.js
+++ src/components/organisms/HousesList/index.js
@@ -4,20 +4,25 @@ import { HouseCard } from '../../molecules';
 
 import { HousesListContainer } from './styles';
 
-export const HousesList = ({ data, loading, children }) => {
+export const HousesList = ({ data, loading, children, onEndReached }) => {
   return (
     <HousesListContainer
       data={data}
       keyExtractor={item => item.property_id}
       ListHeaderComponent={children}
       refreshing={loading}
+      onEndReached={onEndReached}
       renderItem={({ item }) => (
         <HouseCard
           item={item}
           title={item.address.line}
           description={`${item.address.neighborhood_name} - ${item.address.state}`}
           imgSource={item.photos[0].href}
-          price={item.community.price_max}
+          price={
+            item.community.price_max
+              ? item.community.price_max
+              : item.community.price_min
+          }
         />
       )}
     />
